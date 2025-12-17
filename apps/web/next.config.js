e454d6fea21e0d3d15a8f056ead736d4cf894d22
Sarthak Italiya <sarthak.italiya2005@gmail.com>
2025-12-18 01:00:03 +0530

    fixing prod error

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,10 +1,17 @@
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
   reactStrictMode: false,
+  serverExternalPackages: ['@prisma/client', '@bloom/db'],
+  outputFileTracingRoot: path.join(__dirname, '../../'),
 };
 
 export default nextConfig;
